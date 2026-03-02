9e4c65336a6092e94ef6a1b23f7ec8faa018e0bf
ozrvv <ozrv.mail@gmail.com>
2026-03-02 13:40:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ function formatTime(time) {
   const seconds = Math.floor(time % 60) || 0;
   return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
+
+// Scroll reveal
+const sections = document.querySelectorAll(".section");
+
+const revealOnScroll = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  sections.forEach(section => {
+    const boxTop = section.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      section.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
